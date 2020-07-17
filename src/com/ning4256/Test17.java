4e811fe47b2274715d30174c644df4bcf92feb6f
xiaofandegeng <434962239@qq.com>
2020-07-17 23:30:24 +0800

    明天再写

---
+++ src/com/ning4256/Test17.java
@@ -0,0 +1,20 @@
+package com.ning4256;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Test17 {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new LinkedList<>();
+        int index = 0;
+        int n = digits.length();
+        Map<Character, char[]> map = new HashMap<>();
+        map.put('2', new char[]{'a', 'b','c'});
+
+
+
+        return res;
+    }
+}
