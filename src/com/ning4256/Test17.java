a7720c35ec92b5e32379986df3f03fcb58a6081c
xiaofandegeng <434962239@qq.com>
2020-07-18 05:00:23 +0800

    解决17. 电话号码的字母组合

--- src/com/ning4256/Test17.java
+++ src/com/ning4256/Test17.java
@@ -8,13 +8,35 @@ import java.util.Map;
 public class Test17 {
     public List<String> letterCombinations(String digits) {
         List<String> res = new LinkedList<>();
-        int index = 0;
-        int n = digits.length();
+        int cur = 0;
+        if(digits == null || digits.length() < 1)
+            return res;
         Map<Character, char[]> map = new HashMap<>();
         map.put('2', new char[]{'a', 'b','c'});
+        map.put('3', new char[]{'d', 'e','f'});
+        map.put('4', new char[]{'g', 'h','i'});
+        map.put('5', new char[]{'j', 'k','l'});
+        map.put('6', new char[]{'m', 'n','o'});
+        map.put('7', new char[]{'p', 'q','r', 's'});
+        map.put('8', new char[]{'t', 'u','v'});
+        map.put('9', new char[]{'w', 'x','y', 'z'});
 
-
-
+        change("", cur, digits, map, res);
         return res;
+
+    }
+
+    private void change(String s, int cur, String digits, Map<Character, char[]> map, List<String> res) {
+        if(cur == digits.length()){
+            res.add(s);
+        }
+        else {
+            char c = digits.charAt(cur);
+            if (map.containsKey(c)) {
+                for (char ch: map.get(c)) {
+                    change(s + ch, cur + 1, digits, map, res);
+                }
+            }
+        }
     }
 }
