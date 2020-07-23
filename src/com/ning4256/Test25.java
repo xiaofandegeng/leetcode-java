24e793aa541212a53787d3d5fe9c981795bdbf9d
xiaofandegeng <434962239@qq.com>
2020-07-23 22:54:14 +0800

    解决25. K 个一组翻转链表

--- src/com/ning4256/Test25.java
+++ src/com/ning4256/Test25.java
@@ -1,27 +1,29 @@
 package com.ning4256;
 
+import sun.swing.plaf.synth.DefaultSynthStyle;
+
 public class Test25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null) {
-            return head;
-        }
+        if (head == null) return null;
 
         ListNode dummy = new ListNode(0);
-        ListNode prenode = dummy;
+        ListNode prenode =dummy;
         dummy.next = head;
+
         while (prenode != null) {
-            prenode = reverse(prenode, k);
+            prenode = change(prenode, k);
         }
+
         return dummy.next;
+
     }
 
-    private ListNode reverse(ListNode prenode, int k) {
+    private ListNode change(ListNode prenode, int k) {
         ListNode lastnode = prenode;
         for (int i = 0; i < k + 1; i++) {
             lastnode = lastnode.next;
-            if (i != k && lastnode == null) {
+            if(i != k && lastnode == null)
                 return null;
-            }
         }
         ListNode tail = prenode.next;
         ListNode curr = prenode.next.next;
