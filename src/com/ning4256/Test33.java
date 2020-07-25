48f405ea28389a2cbcf13b8b2d30cf6fb78cf864
xiaofandegeng <434962239@qq.com>
2020-07-25 13:59:02 +0800

    解决33. 搜索旋转排序数组

---
+++ src/com/ning4256/Test33.java
@@ -0,0 +1,34 @@
+package com.ning4256;
+
+public class Test33 {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length < 1){
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end- start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[start] < nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid;
+                }else {
+                    start = mid;
+                }
+            }else {
+                if(nums[mid] <= target && target <= nums [end]) {
+                    start = mid;
+                }else {
+                    end = mid;
+                }
+            }
+        }
+
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
+    }
+}
