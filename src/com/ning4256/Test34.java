5bbf2431acaefad3af86b5187c6ef6e570037d14
xiaofandegeng <434962239@qq.com>
2020-07-27 22:54:13 +0800

    解决34. 在排序数组中查找元素的第一个和最后一个位置

--- src/com/ning4256/Test34.java
+++ src/com/ning4256/Test34.java
@@ -4,15 +4,18 @@ public class Test34 {
     public int[] searchRange(int[] nums, int target) {
         //判断nums不为空
         int[] res = new int[] {-1, -1};
-        if(nums == null || nums.length == 0) {
+        //设置初始和结束坐标
+        int st_point = -1, en_point = -1;
+        //设置两个坐标记录位置
+        int start = 0, end = nums.length - 1;
+
+        //排除初始为空
+        if (nums == null || nums.length < 1) {
             return res;
         }
 
-        //设置初始位置和结束位置
-        int st_point = -1, en_point = -1; // 结果记录的点
-        int start = 0, end = nums.length - 1; //移动的俩个坐标
-        //先找到起始坐标的值st_point
-        while (start + 1 < end) {
+        //利用二分法获得初始位置的坐标
+        while (start + 1 < end){
             int mid = start + (end - start) / 2;
             if(nums[mid] >= target) {
                 end = mid;
@@ -20,17 +23,18 @@ public class Test34 {
                 start = mid;
             }
         }
-        //排除第一个是target
-        if (nums[start] == target) {
+        //排除初始位置是target值
+        if(nums[start] == target) {
             st_point = start;
         }else if (nums[end] == target) {
             st_point = end;
         }
 
+        //重置起始位置和结束位置
         start = 0;
         end = nums.length - 1;
-        //先找到结束坐标的值st_point
-        while (start + 1 < end) {
+        //利用二分法获得结束位置的坐标
+        while (start + 1 < end){
             int mid = start + (end - start) / 2;
             if(nums[mid] > target) {
                 end = mid;
@@ -38,13 +42,12 @@ public class Test34 {
                 start = mid;
             }
         }
+        //排除结束位置是target值
         if (nums[end] == target) {
             en_point = end;
-        } else if (nums[start] == target) {
+        }else if(nums[start] == target) {
             en_point = start;
         }
-
-        //返回结果
         res[0] = st_point;
         res[1] = en_point;
         return res;
