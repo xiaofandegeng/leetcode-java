3fb52e26afafcab33d097d85b3cb64b45a1e675f
xiaofandegeng <434962239@qq.com>
2020-07-28 21:16:59 +0800

    解决35. 搜索插入位置

---
+++ src/com/ning4256/Test35.java
@@ -0,0 +1,28 @@
+package com.ning4256;
+
+public class Test35 {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length < 1) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] > target) {
+                end = mid;
+            }else {
+                start = mid;
+            }
+        }
+        if(nums[end] < target) {
+            return end + 1;
+        }
+        if(nums[start] >= target) {
+            return start;
+        }
+        return end;
+    }
+}
