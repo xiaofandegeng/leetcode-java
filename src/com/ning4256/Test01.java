a5bc8b12f1d68a37924a8e4d77df1e6621cbc29a
xiaofandegeng <434962239@qq.com>
2020-07-28 20:35:38 +0800

    解决1. 两数之和

--- src/com/ning4256/Test01.java
+++ src/com/ning4256/Test01.java
@@ -5,19 +5,22 @@ import java.util.Map;
 
 public class Test01 {
     public int[] twoSum(int[] nums, int target) {
+        //设置结果
+        int[] res = new int[] {-1, -1};
+        //判断如果nums为空或者长度不够返回
+        if (nums == null || nums.length < 2) {
+            return res;
+        }
+        // 用map来装位置
         Map<Integer, Integer> map = new HashMap<>();
-        int[] res = new int[2];
-        int val = 0;
-
         for (int i = 0; i < nums.length; i++) {
-            val = target - nums[i];
-            if (map.containsKey(val)) {
-
-                res[1] = i;
+            //利用val是否存在map里面来找到这个值
+            int val = target - nums[i];
+            if(map.containsKey(val)) {
                 res[0] = map.get(val);
+                res[1] = i;
             }
             map.put(nums[i], i);
-
         }
         return res;
     }
